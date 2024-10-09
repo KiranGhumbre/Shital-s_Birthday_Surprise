1cb5e91d21948bfa88dd314ad82348ca9c9af46b
KiranGhumbre <132565139+KiranGhumbre@users.noreply.github.com>
2024-10-09 10:06:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var lgoin=document.querySelector("#login");
+var user=document.querySelector("#e-mail");
+var password=document.querySelector("#password");
+
+lgoin.addEventListener("click",loginSuccess);
+
+
+function loginSuccess(){
+   if(user.value==="Shital-Rathod" && password.value==="10-10-2000"){
+    alert("Happy Birthday Shital! Login Successful");
+    window.location.href="./Profile.html"
+   }
+   else{
+    alert("Incorrect e-mail or password");
+   }
+}
+
+
