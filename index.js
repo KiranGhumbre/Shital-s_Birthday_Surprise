8c1a204918eb97add01ee7906d133602dcda670f
KiranGhumbre <132565139+KiranGhumbre@users.noreply.github.com>
2024-10-11 12:45:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@
 var lgoin=document.querySelector("#login");
 var user=document.querySelector("#e-mail");
 var password=document.querySelector("#password");
-var form=document.querySelector("#myform");
 
 lgoin.addEventListener("click",loginSuccess);
 
 
 function loginSuccess(){
    if(user.value==="Shital-Rathod" && password.value==="10-10-2000"){
-    alert("Happy Birthday Shital! Login Successful");
-    window.location.href="./Profile.html"
+    alert("Sorry! Kiran has restricted you from this page, Please ask for his permission.");
+   // window.location.href="./Profile.html"
    }
    else{
     alert("Incorrect e-mail or password");
